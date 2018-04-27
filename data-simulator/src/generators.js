ae04e152436886aecf730228b353633d2cf9cf89
Elliot Davies <elliot.davies@the-times.co.uk>
2018-04-27 11:39:52 +0100

    tempRange

--- data-simulator/src/generators.js
+++ data-simulator/src/generators.js
@@ -1,6 +1,15 @@
 const faker = require('faker');
 const uuid = require('uuid');
 
+const {
+  startOfToday,
+  endOfToday,
+  differenceInMinutes,
+  addMinutes
+} = require('date-fns');
+
+const { last } = require('ramda');
+
 /**
   Event schema:
 
@@ -15,17 +24,46 @@ const event = category => payload => ({
   uuid: uuid.v4(),
   category,
   payload
+  // @TODO timestamp
 });
 
 // Generate a 'name' event (for testing)
 const generateName = () => faker.name.findName();
 
 // Generate temperature data
-const generateTemp = () => {
-  const min = 5; // °C
-  const max = 25;
+// const generateTemp = () => {
+//   const min = 5; // °C
+//   const max = 25;
+//
+//   return faker.random.number({ min, max });
+// };
 
-  return faker.random.number({ min, max });
+// Generate a temperature event every five minutes for 24 hours
+const generateTempRange = () => {
+  const start = startOfToday();
+  const end = endOfToday();
+  const initialTemp = 5; // °C
+  const interval = 5; // minutes
+
+  const numEvents = Math.round(differenceInMinutes(end, start) / interval);
+  const range = Array(numEvents).fill(null);
+
+  const initialEvent = event('temp')({ time: start, temp: initialTemp });
+
+  return range.reduce(
+    events => {
+      const { payload } = last(events);
+      const { temp, time } = payload;
+
+      const newEvent = event('temp')({
+        temp: temp + 0.1,
+        time: addMinutes(time, 5)
+      });
+
+      return [...events, newEvent];
+    },
+    [initialEvent]
+  );
 };
 
 // Generate an event of the given category
@@ -35,8 +73,10 @@ const generate = category => {
   switch (category) {
     case 'name':
       return makeEvent(generateName());
-    case 'temp':
-      return makeEvent(generateTemp());
+    // case 'temp':
+    //   return makeEvent(generateTemp());
+    case 'tempRange':
+      return generateTempRange();
     default:
       console.error('Bad category', category);
   }
