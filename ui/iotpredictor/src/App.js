8818fcecbf5be8e6b89eb421080c20e5ee1b1c18
Adele Kufour <adele.kufour@news.co.uk>
2018-04-27 14:40:47 +0100

    ui for the data simulator

---
+++ ui/iotpredictor/src/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import { VictoryChart, VictoryTheme, VictoryArea, VictoryAxis } from "victory";
+import "./App.css";
+import Header from "./Header";
+
+const data = [
+  { time: "12:05pm", temp: 20 },
+  { time: "13:05pm", temp: 22 },
+  { time: "14:05pm", temp: 23 },
+  { time: "15:05pm", temp: 22 }
+];
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        {/* chart implementation*/}
+        <div>
+          <Header title="IOT Predictor" />
+        </div>
+        <div>
+          <svg style={{ height: 0 }}>
+            <defs>
+              <linearGradient id="myGradient">
+                <stop offset="0%" stopColor="yellow" />
+                <stop offset="25%" stopColor="yellow" />
+                <stop offset="50%" stopColor="red" />
+                <stop offset="75%" stopColor="gold" />
+              </linearGradient>
+            </defs>
+          </svg>
+          <VictoryChart>
+            <VictoryArea
+              style={{
+                data: { fill: "url(#myGradient)" }
+              }}
+              data={data}
+              x="time"
+              y="temp"
+            />
+          </VictoryChart>
+        </div>
+        <h3> x= time, y= temperature(degrees celcius)</h3>
+      </div>
+    );
+  }
+}
+
+export default App;
