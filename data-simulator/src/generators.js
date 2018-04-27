4477a02f45279a54c9727677e3fb6fbfe611dad1
Elliot Davies <elliot.davies@the-times.co.uk>
2018-04-27 10:36:30 +0100

    Better event generation

--- data-simulator/src/generators.js
+++ data-simulator/src/generators.js
@@ -1,11 +1,36 @@
 const faker = require('faker');
+const uuid = require('uuid');
+
+/**
+  Event schema:
+
+  {
+    id: Uuid,
+    category: 'name' | 'temp' | ...,
+    payload: { ... }
+  }
+
+*/
+const event = category => payload => ({
+  uuid: uuid.v4(),
+  category,
+  payload
+});
 
 // Generate a 'name' event
 const generateName = () => faker.name.findName();
 
-// Mapping of categories to generators
-const generators = {
-  name: generateName
+// Generate an event of the given category
+const generate = category => {
+  const makeEvent = event(category);
+
+  switch (category) {
+    case 'name':
+      return makeEvent(generateName());
+      break;
+    default:
+      console.error('Bad category', category);
+  }
 };
 
-module.exports = category => generators[category]();
+module.exports = generate;
